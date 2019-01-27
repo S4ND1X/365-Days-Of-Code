7fba7ec915811e1789627f24b136e941814af1b7
S4ND1X <42609763+S4ND1X@users.noreply.github.com>
2019-01-26 21:20:28 -0600

    Day 20: Cleaning the tweets

---
+++ Day 20/README.md
@@ -0,0 +1,13 @@
+# Day 19
+
+I was able to split the tweets, so I can receive what I want.
+The problem right now is the way that I am receiving them.
+If you see the other picture, some character doesn't print in right form. 
+I guess that is an encoding issue. I will try to fix it tomorrow, but we are making progress.
+
+
+
+
+
+
+
---
+++ Day 20/getComments.py
@@ -0,0 +1,40 @@
+from tweepy import OAuthHandler
+from tweepy import API
+from tweepy import Cursor
+from datetime import datetime, date, time, timedelta
+from collections import Counter
+import sys
+import tweepy
+import csv
+
+consumer_key = 'UP4MwbLKrDclqSC3EpkRph6Pd'
+consumer_secret = '1YokukdNJCi3VAM7qIOzSrBjOg06Lpjx2W3ehEjfrPeEQKW5Kj'
+access_token = '939919094-PA76fcK91fIjoaN1LASGYdKlwkroN2TJWwIRiwbg'
+access_token_secret = 'GqvN4ZR3O5aY4VEo37xugez8zUCn5kNTaMZUcuJuv0ZZy' 
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+#!Open/create csv file
+csvFile = open('tweets.csv','a')
+#!Use Csv Writer
+csvWriter = csv.writer(csvFile)
+#!Ask how many tweets do you want and who
+user = input("User: ")
+cant = int(input("Number Of Tweets: "))
+#!Search the tweets in the user time line
+userFeed = tweepy.Cursor(api.user_timeline, screen_name=user).items(cant)
+#!Iterate trough the tweets
+for tweets in userFeed:
+   #!Get individual tweet
+   tweet = (tweets.text.encode('utf8'))
+   tweet = str(tweet) 
+   if "Reporte" in tweet:
+     #!Get word index
+     subIndex = tweet.index("Reporte")
+     #!Print everything before the word
+     cleanTweet = tweet[:subIndex:]
+      #!Add clean tweets to csv file
+     csvWriter.writerow([cleanTweet])
+csvFile.close()

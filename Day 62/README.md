6b02734bf3d38a4952b847f5a836d323c8e0f66a
S4ND1X <42609763+S4ND1X@users.noreply.github.com>
2019-03-09 23:07:02 -0600

    Day 62: Trees C++

---
+++ Day 62/README.md
@@ -0,0 +1,7 @@
+#Day 62
+Today I took a break from Web Development to learn Trees.
+They are very fun but more complex than Linked list or Stacks.
+I promise that tomorrow I will make a huge advance to the website.
+
+
+
---
+++ Day 62/tree.cpp
@@ -0,0 +1,102 @@
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+struct nodo{
+     int nro;
+     struct nodo *izq, *der;
+};
+
+typedef struct nodo *ABB;
+/* es un puntero de tipo nodo que hemos llamado ABB, que ulitizaremos
+   para mayor facilidad de creacion de variables */
+
+ABB crearNodo(int x)
+{
+     ABB nuevoNodo = new(struct nodo);
+     nuevoNodo->nro = x;
+     nuevoNodo->izq = NULL;
+     nuevoNodo->der = NULL;
+
+     return nuevoNodo;
+}
+void insertar(ABB &arbol, int x)
+{
+     if(arbol==NULL)
+     {
+           arbol = crearNodo(x);
+     }
+     else if(x < arbol->nro)
+          insertar(arbol->izq, x);
+     else if(x > arbol->nro)
+          insertar(arbol->der, x);
+}
+
+void preOrden(ABB arbol)
+{
+     if(arbol!=NULL)
+     {
+          cout << arbol->nro <<" ";
+          preOrden(arbol->izq);
+          preOrden(arbol->der);
+     }
+}
+
+void enOrden(ABB arbol)
+{
+     if(arbol!=NULL)
+     {
+          enOrden(arbol->izq);
+          cout << arbol->nro << " ";
+          enOrden(arbol->der);
+     }
+}
+
+void postOrden(ABB arbol)
+{
+     if(arbol!=NULL)
+     {
+          postOrden(arbol->izq);
+          postOrden(arbol->der);
+          cout << arbol->nro << " ";
+     }
+}
+
+void verArbol(ABB arbol, int n)
+{
+     if(arbol==NULL)
+          return;
+     verArbol(arbol->der, n+1);
+
+     for(int i=0; i<n; i++)
+         cout<<"   ";
+     cout<< arbol->nro <<endl;
+     verArbol(arbol->izq, n+1);
+}
+int main()
+{
+    ABB arbol = NULL;   // creado Arbo
+    int n;  // numero de nodos del arbol
+    int x; // elemento a insertar en cada nodo
+    cout << "\n\t\t  ..[ ARBOL BINARIO DE BUSQUEDA ]..  \n\n";
+    cout << " Numero de nodos del arbol:  ";
+    cin >> n;
+    cout << endl;
+    for(int i=0; i<n; i++)
+    {
+        cout << " Numero del nodo " << i+1 << ": ";
+        cin >> x;
+        insertar( arbol, x);
+    }
+    cout << "\n Mostrando ABB \n\n";
+    verArbol( arbol, 0);
+    cout << "\n Recorridos del ABB";
+    cout << "\n\n En orden   :  ";   enOrden(arbol);
+    cout << "\n\n Pre Orden  :  ";   preOrden(arbol);
+    cout << "\n\n Post Orden :  ";   postOrden(arbol);
+    cout << endl << endl;
+    system("pause");
+    return 0;
+}
+

5e8fa0996e660703fb7ecbf98aabffa7f0e72e6a
S4ND1X <sandix390@gmail.com.com>
2019-03-16 21:44:12 -0600

    Day 69: Starting with Unity

---
+++ Day 69/README.md
@@ -0,0 +1,11 @@
+#Day 69
+Today I had to reinstall everything on my computer.
+The good part is that it faster and I was able to clean all the system.
+I started with a new project so I can have two main projects to upload.
+I will try to make a Survival Game, right now I'm working on the player movement.
+It's really fun because you can visualize much more what your code does
+Tomorrow I'll be finishing the movement and start with the environment.
+
+
+
+
---
+++ Day 69/Survival Game/Assets/Scripts/playerMotor.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+//Se necesita el rigid body
+[RequireComponent(typeof(Rigidbody))]
+
+public class playerMotor : MonoBehaviour{
+
+    [SerializeField]
+    private Camera cam;
+        
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 cameraRotation = Vector3.zero;
+
+
+
+    private Rigidbody rigBod;
+
+    private void Start()
+
+    {
+        //Indicando que necesita un RigidBody para funcionar
+        rigBod = GetComponent<Rigidbody>();
+    }
+
+
+    //Obtiene un vector de movimiento
+    public void Move (Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+    //Obtiene un vector de rotacion
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+    //Obtiene un vector de rotacion de la camara
+    public void RotateCamera(Vector3 _cameraRotation)
+    {
+        cameraRotation = _cameraRotation;
+    }
+
+    //Hace las fisicas cada cierto tiempo
+    private void FixedUpdate()
+    {
+        PerfomMovement();
+        PerformRotation();
+    }
+
+    //Movimiento segun la velocidad
+    void PerfomMovement()
+    {
+        if(velocity != Vector3.zero)
+        {
+            //Hara que se mueva en la posicion + la velocidad
+            rigBod.MovePosition(rigBod.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+    //Rotacion segun la velocidad.
+    void PerformRotation ()
+    {
+        //Quaternion se usa para rotacion ya procesada por Unity
+        rigBod.MoveRotation(rigBod.rotation * Quaternion.Euler (rotation));
+        if(cam != null)
+        {
+            cam.transform.Rotate(-cameraRotation);           
+            
+        }
+    }
+
+}

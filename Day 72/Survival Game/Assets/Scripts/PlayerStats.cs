ec88a1a0f28a41d3b75834e60f0ee86ea4560c2d
S4ND1X <42609763+S4ND1X@users.noreply.github.com>
2019-03-19 23:53:48 -0600

    Day 72: Player Stats

---
+++ Day 72/Survival Game/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStats : MonoBehaviour
+{
+    //Salud
+    [SerializeField]
+    private float Healt;
+    [SerializeField]
+    private float healtOverTime;
+
+    //Hambre
+    [SerializeField]
+    private float Hunger;
+    [SerializeField]
+    private float HungerOverTime;
+
+    //Sed
+    [SerializeField]
+    private float Thirst;
+    [SerializeField]
+    private float thirstOverTime;
+    //Estamina
+    [SerializeField]
+    private float Stamina;
+    [SerializeField]
+    private float staminaOverTime;
+
+
+    //Sliders
+    [SerializeField]
+    private Slider HealtBar;
+    [SerializeField]
+    private Slider HungerBar;
+    [SerializeField]
+    private Slider ThirstBar;
+    [SerializeField]
+    private Slider StaminaBar;
+
+    [SerializeField]
+    //Valor minimo antes de que empiece a afectar a la salud.
+    private float minAmount = 5f;
+    private float sprintSpeed = 5f;
+
+    Rigidbody rigBod;
+
+    private void Start()
+    {
+        //Obtener el componente RigidBody
+        rigBod = GetComponent<Rigidbody>();
+        //El valor maximo del slider sera el valor inicial, que esta en 100;
+        HealtBar.maxValue = Healt;
+        HungerBar.maxValue = Hunger;
+        ThirstBar.maxValue = Thirst;
+        StaminaBar.maxValue = Stamina;
+
+        updateUI();
+
+
+    }
+
+ 
+    private void Update()
+    {
+        calculateValues();
+    }
+
+    private void calculateValues()
+    {
+        //Pierde Hambre y Sed con el tiempo
+        Hunger -= HungerOverTime * Time.deltaTime;
+        Thirst-= thirstOverTime * Time.deltaTime;
+
+        //Si tiene menos del minimo, empezara a perder vida y Stamina
+        if(Hunger <= minAmount || Thirst <= minAmount)
+        {
+            Healt -= healtOverTime * Time.deltaTime;
+            Stamina -= staminaOverTime * Time.deltaTime;
+        }
+
+        //Si la velocidad en x es mayor a lo "permitido" empezara a perder stamina, salud y sed.
+        if (rigBod.velocity.magnitude >= sprintSpeed && rigBod.velocity.y == 0)
+        {
+            //Le bajara el doble si es que corre. 
+            Stamina -= staminaOverTime * Time.deltaTime;
+            Hunger -= HungerOverTime * Time.deltaTime;
+            Thirst -= thirstOverTime * Time.deltaTime;
+        }
+        else
+        {
+            //Si no esta corriendo, se recupera la estamina a la mitad de lo que la perdio
+            Stamina += (staminaOverTime/2) * Time.deltaTime;
+        }
+
+        if(Healt <= 0)
+        {
+            Debug.Log("Has Muerto");
+        }
+        updateUI();
+    }
+    private void updateUI()
+    {
+        //Restringir los valores entre 0 y un maximo
+        Healt = Mathf.Clamp(Healt, 0, 100f);
+        Hunger = Mathf.Clamp(Hunger, 0, 100f);
+        Thirst = Mathf.Clamp(Thirst, 0, 100f);
+        Stamina = Mathf.Clamp(Stamina, 0, 100f);
+        //Actualiza la barra
+        HealtBar.value = Healt;
+        HealtBar.value = Healt;
+        HungerBar.value = Hunger;
+        ThirstBar.value = Thirst;
+        StaminaBar.value = Stamina;
+    }
+    public void takeDamage(float amount)
+    {
+        Healt -= amount;
+        updateUI();
+    }
+}
---
+++ Day 72/Survival Game/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+//Requiere el Motor para servir
+[RequireComponent(typeof(playerMotor))]
+
+public class playerMovement : MonoBehaviour{
+    //Se Hace SerializedFiled para que este en el inspector pero que siga pudiendo ser privada.
+    [SerializeField]
+    private float speed = 50f;
+    [SerializeField]
+    private float lookSensitivity = 3f;
+
+    [SerializeField]
+    private float thrusterForce = 1000f;
+
+    private playerMotor motor;
+
+
+    private void Start()
+    {
+        //Indica que el  motor para el Player sera el componente motor.
+        motor = GetComponent<playerMotor>();
+    }
+
+    private void Update()
+    {
+        //Calcular el movimiento del Jugador con un vector 3D
+        //Se pide GetAxisRaw, para que no se haga preprocesamiento
+        float _xMovement = Input.GetAxisRaw("Horizontal");
+        float _zMovement = Input.GetAxisRaw("Vertical");
+        //Se movera a la derecha tomando en cuenta su previa posicion, la cantidad de movimiento dicha.
+        //Tambien logra tomar valores negativos en los ejes
+        Vector3 moveHorizontal = transform.right * _xMovement;
+        Vector3 moveVertical = transform.forward * _zMovement;
+        
+        /*Se hace normalized para hacerlo = 1 y que no tenga una velocidad ridicula y solo indicara una direccion
+         Y luego se multiplica por nuestra velocidad declarada al inicio */
+        Vector3 _velocity = (moveHorizontal + moveVertical).normalized * speed;
+
+        //Aplicar el movimiento
+        motor.Move(_velocity);
+
+        //Calcular la rotacion como Vector 3D (Volteando alrededor)
+        float _yRota = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, _yRota, 0f) * lookSensitivity;
+
+        //Aplicar la rotacion
+        motor.Rotate(_rotation);
+
+       
+        //Calcular la rotacion de la camara como Vector 3D (Volteando alrededor)
+        float _xRota = Input.GetAxisRaw("Mouse Y");
+
+        float _cameraRotationX = _xRota * lookSensitivity;
+        //Aplicar la rotacion
+        motor.RotateCamera(_cameraRotationX);
+
+        //La fuerza sera 0 si no hay nada
+        Vector3 _thrusterForce = Vector3.zero;
+        
+        //Calcular la fuerza
+        if (Input.GetButton("Jump"))
+        {
+            //Se hara un vector hacia arriba multiplicado por la fuerza de salto
+            _thrusterForce = Vector3.up * thrusterForce;
+        }
+        //Aplicar el salto 
+        motor.ApplyThruster(_thrusterForce);
+    }
+
+}

d6b55ef635c27da2e14dd375db097587cdfd8797
S4ND1X <42609763+S4ND1X@users.noreply.github.com>
2019-03-21 23:12:51 -0600

    Day 74: Starting with Unity Basics

---
+++ Day 74/Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerController : MonoBehaviour
+{
+    //Se crea la variable del tipo componente que necesites referenciar
+    private Rigidbody rb;
+    [SerializeField]
+    private float movementSpeed = 0f;
+    private void Start()
+    {
+        //Aqui se iguala la variable a su componente.
+        rb = GetComponent<Rigidbody>();        
+    }    
+    private void FixedUpdate()
+    {
+        //Obtener el Input de movimiento
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        //Crear el vector de movimiento,(X,Y,Z)
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        //AddForce recibe un vector y un modode fuerza
+        rb.AddForce(movement*movementSpeed);
+    }
+}

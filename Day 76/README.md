7faba7b1298e54cd9d7965c55e72c2c27f4a0d0a
S4ND1X <42609763+S4ND1X@users.noreply.github.com>
2019-03-23 23:14:49 -0600

    Day 76: Collision

---
+++ Day 76/README.md
@@ -0,0 +1,7 @@
+#Day 76
+Today I learned how to make a simple animation.
+I also spent the whole day learning about colliders and how do they work.
+I didn't know about static and dynamic colliders so I will try to apply it from now on.
+
+
+
---
+++ Day 76/Roll a Ball/Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+   
+    void Update()
+    {
+        transform.Rotate(new Vector3 (90,45,90)*Time.deltaTime);
+    }
+}

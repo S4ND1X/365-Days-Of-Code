b9be58bd6808c1976ceaf8525580697bc239ed12
S4ND1X <42609763+S4ND1X@users.noreply.github.com>
2019-03-24 19:42:07 -0600

    Day 77: Text UI

---
+++ Day 77/README.md
@@ -0,0 +1,5 @@
+#Day 77
+Today I learned a lot about UI in unity.
+I read a lot, on how to modify elements in UI, using code.
+It is easy, but you have to read a lot of the documentation to really make cool things.
+Tomorrow I finish this project and start the new tutorial!
---
+++ Day 77/Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerController : MonoBehaviour
+{
+    //Se crea la variable del tipo componente que necesites referenciar
+    private Rigidbody rb;
+    [SerializeField]
+    private float movementSpeed = 0f;
+    [SerializeField]
+    //Se crea para contener la referencia
+    private Text countText;
+    [SerializeField]
+    private Text winText;
+    private int count;
+    private void Start()
+    {
+        //Aqui se iguala la variable a su componente.
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        //A que se igualara el texto a desplegar
+        winText.text = "";
+        setCountText();
+    }    
+    private void FixedUpdate()
+    {
+        //Obtener el Input de movimiento
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        //Crear el vector de movimiento,(X,Y,Z)
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        //AddForce recibe un vector y un modode fuerza
+        rb.AddForce(movement*movementSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            countText.text = "Score: " + count.ToString();
+        }
+    }
+
+    void setCountText()
+    {
+        countText.text = "Score: " + count.ToString();
+        if(count == 8)
+        {
+            winText.text = "You have won";
+        }
+    }
+}

738db74566594931bcd9bff8e98b6ea4e2e00b66
S4ND1X <42609763+S4ND1X@users.noreply.github.com>
2019-03-26 22:45:21 -0600

    Day 79: Chocolate Cake HR

---
+++ Day 79/Chocolate Cake.cs
@@ -0,0 +1,36 @@
+using System;
+using static System.Console;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        ReadLine();
+        var s_temp = Console.ReadLine().Split(' ');
+        var choclateBarValues = Array.ConvertAll(s_temp, Int32.Parse);
+        var tokens_d = Console.ReadLine().Split(' ');
+        var birthday = Convert.ToInt32(tokens_d[0]);
+        var birthMonth = Convert.ToInt32(tokens_d[1]);
+        var result = Solve(choclateBarValues, birthday, birthMonth);
+        WriteLine(result);
+    }
+    static int Solve(int[] choclateBarValues, int birthday, int birthMonth)
+    {
+        var totalWays = 0;
+        if (choclateBarValues.Length >= birthMonth)
+        {
+            var barPartSum = 0;
+            for (int i = 0; i < birthMonth; i++)
+                barPartSum += choclateBarValues[i];
+            if (barPartSum == birthday)
+                totalWays++;
+            for (int i = 0; i < choclateBarValues.Length - birthMonth; i++)
+            {
+                barPartSum = barPartSum - choclateBarValues[i] + choclateBarValues[i + birthMonth];
+                if (barPartSum == birthday)
+                    totalWays++;
+            }
+        }
+        return totalWays;
+    }
+}
---
+++ Day 79/README.md
@@ -0,0 +1,5 @@
+#Day 79
+Once again I was very busy doing my homework but it was worth the pain. 
+This week I have my exams so I really can not do much until the next weekend, 
+because this weekend I will have to study for my exams. 
+But I will still try to do as much as I can.

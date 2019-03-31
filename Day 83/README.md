3feafd750c4bbc7dfa5bd067fdcdee4070b888f4
S4ND1X <42609763+S4ND1X@users.noreply.github.com>
2019-03-30 22:15:14 -0600

    Day 83: Drawing Book

---
+++ Day 83/README.md
@@ -0,0 +1,2 @@
+#Day 83
+In the morning I studied a little, Segment Tree, but then I had to study physics, the good thing is that it is not long before my exams are over and I can concentrate on the program
---
+++ Day 83/drawingBook.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include <iostream>
+int main()
+{
+    int n=0,p=0,min;
+    scanf("%d",&n);
+    scanf("%d",&p);
+    min=(n/2)-(p/2);
+    if(min>p/2)
+        min=p/2;
+    printf("%d",min);
+    return 0;
+}

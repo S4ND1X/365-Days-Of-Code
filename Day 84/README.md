924cf2f87011b829098a44544533cdf4db425fce
S4ND1X <42609763+S4ND1X@users.noreply.github.com>
2019-03-31 20:02:41 -0600

    Day 84: Dynamic Array C++

---
+++ Day 84/README.md
@@ -0,0 +1,3 @@
+#Day 83
+I think this will be one of the latest hackerrank problems that I will do
+Since I'm going to start studying OOP in Java and data structure, to prepare myself for my class next semester.
---
+++ Day 84/dynamicArray.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() 
+{  
+    int N, Q;
+    cin >> N >> Q;
+    vector<int> seqList[N];
+    int lastAns = 0;
+    for (int i = 0; i < Q; i++) 
+    {
+        int choose, x, y;
+        cin >> choose >> x >> y;
+        int pos = (x ^ lastAns) % N;
+    if (choose == 1) 
+        {
+        seqList[pos].push_back(y);
+        } 
+    else if (choose == 2) 
+        {
+        int index = y % ((int)seqList[pos].size());
+        lastAns = seqList[pos][index];
+        cout << lastAns << endl;
+        }
+    }
+        return 0;
+}

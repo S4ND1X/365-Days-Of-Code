f1faaec6125648a956683acb59578d06ab2ac197
S4ND1X <42609763+S4ND1X@users.noreply.github.com>
2019-04-09 23:05:29 -0500

    Day 93: Maximal Difference

---
+++ Day 93/MaximalDifference.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bits/stdc++.h> 
+
+
+using namespace std;
+
+int main(){
+
+
+    
+    //Length of array
+    n = inputArray.size()
+    //Sort array  
+    sort(inputArray.begin(), inputArray.end()); 
+    //Difference between first, and last element
+    int difference = arr[n-1] - arr[0];
+    return difference;
+
+
+    return 0;
+}

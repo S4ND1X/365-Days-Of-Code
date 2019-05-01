f4108b2783514a06fb99ba881f84510a7e8c1b99
S4ND1X <42609763+S4ND1X@users.noreply.github.com>
2019-04-30 19:12:35 -0500

    Day 114: Open files with Diana

---
+++ Day 114/README.md
@@ -0,0 +1,7 @@
+#Day 113
+This was the first that day I had the chance to sleep.
+The bad side is that I have a lot of homework from the last week.
+I just solved another codesignal problem.
+
+I hope to end my exams really soon so I can code a lot more.
+
---
+++ Day 114/aiuda.py
@@ -0,0 +1,56 @@
+import datetime
+import random
+
+#!Cambiar el dia 
+def dia_Siguiente(str_fecha):
+    fecha=datetime.datetime.strptime(str_fecha,"%d/%m/%y")
+    siguiente=fecha + datetime.timedelta(days=1)
+    return siguiente.strftime("%d/%m/%y")
+
+
+#?Generar el archivo
+def genera_archivo(str_fecha):
+    fecha=str_fecha.replace("/","_")
+    try:
+        archivo=open("Datos_"+fecha+".csv","w",encoding="utf-8")
+    except:
+        print("Error al generar el archivo")
+    else:
+        archivo.write(f"Informe de líneas de producción 7 días con inicio el {str_fecha}\n")
+        for day in range(7):
+            for turno in range(1,4):
+                for linea in range(1,4):
+                    productos = random.randint(200,500)
+                    detuvo = random.randint(0,10)
+                    tiempo = detuvo * random.randint(1,15)
+                    lista = []
+                    lista.append(str(linea))
+                    lista.append(str(turno))
+                    lista.append(str(dia))
+                    lista.append(str(productos))
+                    lista.append(str(detuvo))
+                    lista.append(str(tiempo))
+                    wea = ','.join(lista)
+                    wea = wea + "\n"
+                    archivo.write(wea)
+            dia_Siguiente(dia)
+        archivo.close()
+
+#!Programa principal
+dia=input("Ingresa fecha en formato dia/mes/año: ")
+genera_archivo(dia)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

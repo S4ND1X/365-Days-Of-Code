4c03f702f65812c611616e5707388dc9ee50bc53
S4ND1X <42609763+S4ND1X@users.noreply.github.com>
2019-06-21 21:58:05 -0500

    Day 166: Bugs

---
+++ Day 166/First Game/src/com/miko/main/BasicEnemy.java
@@ -0,0 +1,33 @@
+package com.miko.main;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import com.sun.javafx.geom.Rectangle;
+
+public class BasicEnemy extends GameObject {
+	//Valores Iniciales
+	public BasicEnemy(int x, int y, ID id) {
+		super(x, y, id);		
+		velX = 5;
+		velY = 5;
+	}	
+	
+	public Rectangle getBounds() {
+		return new Rectangle(x, y, 16, 16);
+	}
+	
+	//Elementos a actualizar
+	public void tick() {
+		x += velX;
+		y += velY;
+		if(y <= 0 || y >= Game.HEIGHT - 32) velY *= -1;		
+		if(x <= 0 || x >= Game.WIDTH - 16) velX *= -1;
+	}
+	//Crear elemento grafico
+	public void render(Graphics g) {
+		g.setColor(Color.BLACK);		
+		g.fillRect(x, y, 16, 16);
+	} 
+
+}
---
+++ Day 166/First Game/src/com/miko/main/GameObject.java
@@ -0,0 +1,66 @@
+package com.miko.main;
+
+import java.awt.Graphics;
+
+import com.sun.javafx.geom.Rectangle;
+
+public abstract class GameObject {
+	
+	protected int x, y;
+	protected ID id;
+	protected int velX, velY;
+	
+	
+	public GameObject(int x, int y, ID id) {
+		this.x = x;
+		this.y = y;
+		this.id = id;
+	}
+	
+	public abstract void tick();
+	public abstract void render(Graphics g);
+	public abstract Rectangle getBounds();
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public int getX() {
+		return this.x;
+	}
+	
+	public int getY() {
+		return this.y;
+	}
+	
+	public void setID(ID id) {
+		this.id = id;
+	}
+	
+	public ID getID() {
+		return this.id;
+	}
+	
+	public void setVelX(int velX) {
+		this.velX = velX;
+	}
+	
+	public void setVelY(int velY) {
+		this.velY = velY;
+	}
+	
+	public int getVelX() {
+		return this.velX;
+	}
+	
+	public int getVelY() {
+		return this.velY;
+	}
+	
+	
+	
+}

f7f01a0a0b68320df7987a5d68faa8eca2d6af37
S4ND1X <42609763+S4ND1X@users.noreply.github.com>
2019-07-23 16:05:07 -0500

    Day 198: Scripatble Objects Unity

---
+++ Day 198/EnemyPathing.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    //Config param
+    [SerializeField] private List<Transform> wayPoints;
+    [SerializeField] private float moveSpeed = 3f;
+    private int wayPointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = wayPoints[wayPointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveEnemyOnPath();
+
+    }
+
+    private void MoveEnemyOnPath()
+    {
+        //Si es lista se usa Count en vez de Length
+        if (wayPointIndex <= wayPoints.Count - 1)
+        {
+            var targetPosition = wayPoints[wayPointIndex].transform.position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position,
+                                                        targetPosition,
+                                                        movementThisFrame);
+            if (transform.position.Equals(targetPosition)) wayPointIndex++;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Day 198/README.md
@@ -0,0 +1,4 @@
+#Day 198
+
+Today I learned what ScriptableObjects are and when they should be used.
+I'm also starting a project of a game inspired by the type of BalckThornProd games

d3741f3f61276170d13250ca3450f2feb8a08a01
S4ND1X <42609763+S4ND1X@users.noreply.github.com>
2019-07-29 20:07:34 -0500

    Starting with Monstercards app

---
+++ Day 204/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monster: []
+    };
+  }
+
+  componentDidMount() {
+    //!Hace el request a la pagina, despues convierte esa respuesta en un json
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    //!Despues de convertir en json, asigna a este array lo adquirido
+    .then(users => this.setState({ monster : users }))
+  }
+
+  render() {
+    return this.state.monster.map(monster => (
+      <h1 key={monster.id}> {monster.name} </h1>
+    ));
+  }
+}
+
+export default App;
---
+++ Day 204/README.md
@@ -0,0 +1,3 @@
+#Day 204
+
+I started again with React, this time I will try to devote enough time so I will divide all my efforts between game development and web development. So I will be uploading those two topics for a few months, I think.

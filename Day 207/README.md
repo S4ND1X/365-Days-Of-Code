979546d9e04ac24977c64023a249004d14d92db9
S4ND1X <42609763+S4ND1X@users.noreply.github.com>
2019-08-01 23:30:47 -0500

    Day 207: Scenes and Level Loader

---
+++ Day 207/README.md
@@ -0,0 +1,4 @@
+#Day 207
+
+Being honest, I can't practically program anything today, but I promise that tomorrow I will make significant progress tomorrow.
+This shows what I have done, this is is nothing but tomorrow I will start shaping it.
---
+++ Day 207/Scripts/LevelLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    [SerializeField] private int startScreenIndex;
+    private int currentSceneIndex;
+    [SerializeField] private string gameScreenName;
+    [SerializeField] private string splashScreenName;
+    [SerializeField] private int secondsToWaitAndLoad;
+
+    private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex.Equals(1)) StartCoroutine(WaitForTime());
+    }
+
+    private IEnumerator WaitForTime()
+    {
+            yield return new WaitForSeconds(secondsToWaitAndLoad);
+            LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}

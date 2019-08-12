5d65423e2dc83f2a87c9e7e268b9cf356db95ce7
S4ND1X <42609763+S4ND1X@users.noreply.github.com>
2019-08-11 20:47:10 -0500

    Day 217: Camera Movement

---
+++ Day 217/PlayerLook.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    //Config param
+    [SerializeField]private string mouseXInputName, mouseYInputName;
+    [SerializeField] private float mouseSensitivity;
+    [SerializeField] private Transform playerBody;
+
+    //Restriccion en x
+    private float xAxisClamp;
+
+    private void Awake()
+    {
+        LockCursor();
+        xAxisClamp = 0.0f;
+    }
+
+
+    private void LockCursor()
+    {
+        //bloquea el cursor al centro de la pantalla
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    private void Update()
+    {
+        CamerRotation();
+    }
+
+    private void CamerRotation()
+    {
+        //Obtiene la posicion del mouse
+        float mouseX = Input.GetAxisRaw(mouseXInputName) * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxisRaw(mouseYInputName) * mouseSensitivity * Time.deltaTime;
+
+        xAxisClamp += mouseY;
+
+        //Si esta viendo completamente hacia arriba
+        if (xAxisClamp >= 90.0f)
+        {
+            xAxisClamp = 90.0f;
+            mouseY = 0.0f;
+            ClampXAxisToValue(270.0f);
+        }
+        //Si esta viendo completamente hacia abajo
+        else if (xAxisClamp <= -90.0f)
+        {
+            xAxisClamp = -90.0f;
+            mouseY = 0.0f;
+            ClampXAxisToValue(90.0f);
+        }
+        //Se mueve la rotacion del eje segun el mouse
+        transform.Rotate(Vector3.left * mouseY);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+    }
+
+    //Funcion para restringir movimiento en Y
+    private void ClampXAxisToValue(float value)
+    {
+        //Se restringe el movimiento a cierto valor con los angulos de euler
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
+}
---
+++ Day 217/README.md
@@ -0,0 +1,5 @@
+#Day 217
+
+
+Today I worked on the camera movement for an fps-like game.
+Tomorrow I go back to school, so I don't know how much time I will have left for my personal projects but I will organize the time to have something free.

743b2777b2465d077463d975b7b39dd5f350703f
S4ND1X <42609763+S4ND1X@users.noreply.github.com>
2019-08-20 21:00:58 -0500

    Day 226: Shot gun Unity

---
+++ Day 226/Gun.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform bulletSpawnPoint;
+    [SerializeField] private float bulletSpeed;
+    [SerializeField] private string fireInputName;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetButtonDown(fireInputName))Shoot();
+    }
+
+
+    private void Shoot()
+    {
+        GameObject currentSpawnedBullet;
+        currentSpawnedBullet = Instantiate( bulletPrefab,
+                                            bulletSpawnPoint.position,
+                                            bulletSpawnPoint.rotation) as GameObject;
+
+        Rigidbody bulletTempRB;
+        bulletTempRB = currentSpawnedBullet.GetComponent<Rigidbody>();
+
+        bulletTempRB.AddForce(transform.forward * bulletSpeed);
+
+    }
+}

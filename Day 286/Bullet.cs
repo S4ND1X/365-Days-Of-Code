82600d7b1c77991844b87c7e6a1f034bde1855aa
S4ND1X <42609763+S4ND1X@users.noreply.github.com>
2019-10-21 21:34:14 -0500

    Day 286: Unity Comeback

---
+++ Day 286/Bullet.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    //Config param
+    [SerializeField] private AudioClip[] clips;
+    [SerializeField] private int bulletMaxTime;
+    [SerializeField] private GameObject particle;
+    private int timeSinceBulletSpawned;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayFiringSFX();
+        Destroy(gameObject, bulletMaxTime);
+
+    }
+
+    private void PlayFiringSFX()
+    {
+        //Reproduce el sonido en donde sea el inicio de la bala
+        AudioSource.PlayClipAtPoint(clips[0], transform.position);
+    }
+    private void PlayHitSFX()
+    {
+        AudioSource.PlayClipAtPoint(clips[1], transform.position);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayHitSFX();
+        //ParticlesHit();
+        Destroy(gameObject);
+        Debug.Log("Collision");
+    }
+
+   /* private void ParticlesHit()
+    {
+        Instantiate(particle, transform.position, Quaternion.identity);
+        Destroy(particle,3.0f);
+
+    }
+    */
+}
---
+++ Day 286/README.md
@@ -0,0 +1,2 @@
+#Day 285
+Today I was preparing some workshops for the school as well as I studied for my exam on Wednesday. Once finished that exam I will be able to relax a little

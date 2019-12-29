622a033b3c2438fdaab05ab4065ace3d9f9e15d3
S4ND1X <42609763+S4ND1X@users.noreply.github.com>
2019-12-28 22:59:31 -0600

    Day 328: Enemy spawn

---
+++ Day 328/EnemySpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private List<WaveConfig> waveConfigs;
+    [SerializeField] private bool looping = false;
+    private int startingWave = 0;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        do yield return StartCoroutine(SpawnAllWaves());
+        while (looping);
+    }
+
+    private IEnumerator SpawnAllWaves()
+    {
+        for(int waveIndex = startingWave; waveIndex < waveConfigs.Count; waveIndex++)
+        {
+            //Se empieza con la primera oleada y  se empieza la co-rutina
+            var currentWave = waveConfigs[waveIndex];
+            yield return StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+        }
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
+    {
+        //Crea un enemigo hasta que haya la cantidad deseada por la configuracion
+        for(int i = 0; i < waveConfig.GetNumberOfEnemies(); i++) {
+
+            var newEnemy = Instantiate(waveConfig.GetEnemyPrefab(),
+                    waveConfig.GetWayPoints()[0].transform.position,
+                    Quaternion.identity);
+
+            newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(waveConfig);
+
+            yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawn());
+        }
+
+    }
+}
---
+++ Day 328/README.md
@@ -0,0 +1,2 @@
+#Day 328
+Today I continued investigating the spawn system of the enemies, it is not complicated although the movement is not so sophisticated compared to some pathfinding algorithm

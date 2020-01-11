ce8b43b66ca3d06893211ea4175d2037cf649cc6
S4ND1X <42609763+S4ND1X@users.noreply.github.com>
2020-01-10 23:14:30 -0600

    Day 330: GameSession Unity

---
+++ Day 330/GameSession.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSession : MonoBehaviour
+{
+
+    [SerializeField] private int score = 0;
+
+    private void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    private void SetUpSingleton()
+    {
+        /*Si hay más de un objeto se destruye para que solo uno lleve el control
+         * el otro no se destruye en el cambio de escena
+         */
+        if (FindObjectsOfType(GetType()).Length > 1) Destroy(gameObject);
+        else DontDestroyOnLoad(gameObject);
+        
+
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+
+    public void AddToScore(int scoreValue)
+    {
+        this.score += scoreValue;
+    }
+
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+
+
+}

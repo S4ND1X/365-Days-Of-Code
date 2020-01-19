70c9fd4600a698c3c537dbe556bcbb3f4ab7698e
Jorge <sandix390@gmail.com>
2020-01-18 23:37:44 -0600

    Day 337: Java Sorting

---
+++ Day 337/README.md
@@ -0,0 +1,3 @@
+#Day 337
+
+Today I started to make a sorting algorithm visualizer in java, I want to start practicing and I thought it would be a good project, the bad thing is that today I had to format my computer so I could not use it almost all day, I saw on the internet a way of making the loop to update the display.
---
+++ Day 337/visualizer.java
@@ -0,0 +1,14 @@
+public void preDrawArray(Integer[] squares){
+		squarePanels = new JPanel[SortingVisualizer.sortDataCount];
+		arrayWrapper.removeAll();
+        
+		sizeModifier =  (int) ((getHeight()*0.9)/(squarePanels.length));
+		for(int i = 0; i<SortingVisualizer.sortDataCount; i++){
+			squarePanels[i] = new JPanel();
+			squarePanels[i].setPreferredSize(new Dimension(SortingVisualizer.blockWidth, squares[i]*sizeModifier));
+			squarePanels[i].setBackground(Color.blue);
+			arrayWrapper.add(squarePanels[i], c);
+		}
+		repaint();
+		validate();
+	}
